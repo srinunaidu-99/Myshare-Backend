bf291cfbed319aec516e03bafdc29ee13d97d0d2
srinunaidu-99 <srinunaidu9912173618@gmail.com>
2026-06-14 20:10:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(5000, () => {
-  console.log("🚀 MyShare Server running on 5000");
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
